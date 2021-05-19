34bee2abc6940ce3b1e1501c16749066977fcd1e
tinyOfficeAutomatization <tinyOfficeAutomatization@gmail.com>
2021-05-19 22:06:25 +0300

    -

--- qlua_sma_botUWGN.lua
+++ qlua_sma_botUWGN.lua
@@ -4,16 +4,16 @@
 
 local qlua_sma_botUWGN = { _version = "0.1.1" }
 
--- Р¤Р»Р°Рі РїРѕРґРґРµСЂР¶Р°РЅРёСЏ СЂР°Р±РѕС‚С‹ СЃРєСЂРёРїС‚Р°
+-- Флаг поддержания работы скрипта
 IsRun = true;
 
 log = require "log"
 myqlua = require "myqlua"
 
-local PRICE_STEP = 0.03		-- РѕС‚РєР»РѕРЅРµРЅРёРµ, РєРѕС‚РѕСЂРѕРµ СЃС‡РёС‚Р°РµС‚СЃСЏ РґРѕСЃС‚Р°С‚РѕС‡РЅС‹Рј РґР»СЏ РїРµСЂРµС…РѕРґР° РЅР° СЃР»РµРґСѓСЋС‰РёР№ СѓСЂРѕРІРµРЅСЊ
-local act_list = {} 	-- РјР°С‚СЂРёС†Р° СЃ РёРЅСЃС‚СЂСѓРјРµРЅС‚Р°РјРё
-local order_list = {}	-- СЃРїРёСЃРѕРє Р·Р°СЏРІРѕРє
-local amount_rur = 0			-- РєРѕР»-РІРѕ РґРµРЅРµРі
+local PRICE_STEP = 0.03		-- отклонение, которое считается достаточным для перехода на следующий уровень
+local act_list = {} 	-- матрица с инструментами
+local order_list = {}	-- список заявок
+local amount_rur = 0			-- кол-во денег
 local class_code = "TQBR"
 local PORTFOLIO_FILE = "portUWGN.prt"
 
@@ -25,34 +25,34 @@ function qlua_sma_botUWGN.main()
 	
 	log.trace('-- -- -- '..PORTFOLIO_FILE)
 
-	-- РџС‹С‚Р°РµС‚СЃСЏ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р» РІ СЂРµР¶РёРјРµ "С‡С‚РµРЅРёСЏ/Р·Р°РїРёСЃРё"
+	-- Пытается открыть файл в режиме "чтения/записи"
 	f = io.open(getScriptPath().."\\"..PORTFOLIO_FILE, "r+");
-	-- Р•СЃР»Рё С„Р°Р№Р» РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚
+	-- Если файл не существует
 	if f == nil then 
 		log.trace('creating file')
-		-- РЎРѕР·РґР°РµС‚ С„Р°Р№Р» РІ СЂРµР¶РёРјРµ "Р·Р°РїРёСЃРё"
+		-- Создает файл в режиме "записи"
 		f = io.open(getScriptPath().."\\"..PORTFOLIO_FILE, "w"); 
-		-- Р—Р°РєСЂС‹РІР°РµС‚ С„Р°Р№Р»
+		-- Закрывает файл
 		f:close();
-		-- РћС‚РєСЂС‹РІР°РµС‚ СѓР¶Рµ СЃСѓС‰РµСЃС‚РІСѓСЋС‰РёР№ С„Р°Р№Р» РІ СЂРµР¶РёРјРµ "С‡С‚РµРЅРёСЏ"
+		-- Открывает уже существующий файл в режиме "чтения"
 		f = io.open(getScriptPath().."\\"..PORTFOLIO_FILE, "r");
 	end;
    
-	-- Р—Р°РїРёСЃС‹РІР°РµС‚ РІ С„Р°Р№Р» 2 СЃС‚СЂРѕРєРё
-	--f:write("Line1\nLine2"); -- "\n" РїСЂРёР·РЅР°Рє РєРѕРЅС†Р° СЃС‚СЂРѕРєРё
-	-- РЎРѕС…СЂР°РЅСЏРµС‚ РёР·РјРµРЅРµРЅРёСЏ РІ С„Р°Р№Р»Рµ
+	-- Записывает в файл 2 строки
+	--f:write("Line1\nLine2"); -- "\n" признак конца строки
+	-- Сохраняет изменения в файле
 	--f:flush();
-	-- Р’СЃС‚Р°РµС‚ РІ РЅР°С‡Р°Р»Рѕ С„Р°Р№Р»Р° 
-	  -- 1-С‹Рј РїР°СЂР°РјРµС‚СЂРѕРј Р·Р°РґР°РµС‚СЃСЏ РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ С‡РµРіРѕ Р±СѓРґРµС‚ СЃРјРµС‰РµРЅРёРµ: "set" - РЅР°С‡Р°Р»Рѕ, "cur" - С‚РµРєСѓС‰Р°СЏ РїРѕР·РёС†РёСЏ, "end" - РєРѕРЅРµС† С„Р°Р№Р»Р°
-	  -- 2-С‹Рј РїР°СЂР°РјРµС‚СЂРѕРј Р·Р°РґР°РµС‚СЃСЏ СЃРјРµС‰РµРЅРёРµ
+	-- Встает в начало файла 
+	  -- 1-ым параметром задается относительно чего будет смещение: "set" - начало, "cur" - текущая позиция, "end" - конец файла
+	  -- 2-ым параметром задается смещение
    
-	--log.trace('РЎС‡РёС‚Р°С‚СЊ РґР°РЅРЅС‹Рµ РІ РјР°СЃСЃРёРІ')
-	local a = {} 			-- РѕР±СЉСЏРІР»РµРЅРёРµ РґРѕР»Р¶РЅРѕ Р±С‹С‚СЊ Р·Р° РїСЂРµРґРµР»Р°РјРё С†РёРєР»Р°
-	local n = 1  			-- РёРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј СЃС‡С‘С‚С‡РёРє СЌР»РµРјРµРЅС‚РѕРІ С‚Р°Р±Р»РёС†С‹
+	--log.trace('Считать данные в массив')
+	local a = {} 			-- объявление должно быть за пределами цикла
+	local n = 1  			-- инициализируем счётчик элементов таблицы
 	io.input(getScriptPath().."\\"..PORTFOLIO_FILE) 
 	while true do
-		local line = io.read("*line") -- С‡РёС‚Р°РµРј С†РµР»СѓСЋ СЃС‚СЂРѕРєСѓ
-		if line == nil then break end -- РµСЃР»Рё РЅРёС‡РµРіРѕ РЅРµ РїСЂРѕС‡РёС‚Р°Р»РѕСЃСЊ - РєРѕРЅРµС† С†РёРєР»Р°
+		local line = io.read("*line") -- читаем целую строку
+		if line == nil then break end -- если ничего не прочиталось - конец цикла
 			arr = mysplit(line, ';')
 			a[n] = { arr }
 			
@@ -60,12 +60,12 @@ function qlua_sma_botUWGN.main()
 			--log.trace('line: '..line);
 			--log.trace('a['..n..'][1]:'..arr[1]..' '..'a['..n..'][2]:'..arr[2]..' '..'a['..n..'][3]:'..arr[3]..' ');
 			act_list[n] = {}
-			act_list[n][1] = arr[1];	-- РќР°Р·РІР°РЅРёРµ
-			act_list[n][2] = arr[2];	-- Р›РѕС‚
-			act_list[n][3] = arr[3];	-- Р¦РµРЅР°
+			act_list[n][1] = arr[1];	-- Название
+			act_list[n][2] = arr[2];	-- Лот
+			act_list[n][3] = arr[3];	-- Цена
 			if (act_list[n][1] ~= 'rur') then
-				act_list[n][4] = arr[4];	-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїСЂРѕРґР°Р¶Рё
-				act_list[n][5] = arr[5];	-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїРѕРєСѓРїРєРё
+				act_list[n][4] = arr[4];	-- Номер уровня продажи
+				act_list[n][5] = arr[5];	-- Номер уровня покупки
 			end	
 			--[[log.trace('act_list['..n..'][1]:'..act_list[n][1]..' '
 					..'act_list['..n..'][2]:'..act_list[n][2]..' '
@@ -74,28 +74,28 @@ function qlua_sma_botUWGN.main()
 			
 			
 			n = n + 1;
-		--x,y = string.match(line,"([01]+) (%a)") -- РґРІРѕРёС‡РЅС‹Рµ С†РёС„СЂС‹ - РїРѕРјРµС‰Р°РµРј РІ x, Р±СѓРєРІСѓ - РІ y
-		--a[x]=y -- РґРѕР±Р°РІР»СЏРµРј СЌС‚Сѓ РїР°СЂСѓ РІ Р°СЃСЃРѕС†РёР°С‚РёРІРЅС‹Р№ РјР°СЃСЃРёРІ
+		--x,y = string.match(line,"([01]+) (%a)") -- двоичные цифры - помещаем в x, букву - в y
+		--a[x]=y -- добавляем эту пару в ассоциативный массив
 	end
 	io.input():close()
 	
 	log.trace('length act_list: '..#act_list)			-- remove
 	
-	-- РќР°РґРѕ РїРµСЂРµР±СЂР°С‚СЊ СЌР»РµРјРµРЅС‚С‹ РјР°СЃСЃРёРІР° Рё РїРѕР»СѓС‡РёС‚СЊ РґР»СЏ РЅРёС… С†РµРЅС‹
+	-- Надо перебрать элементы массива и получить для них цены
 	for i = 1, #act_list do
 		log.trace('--: '..act_list[i][1]);
 		if (act_list[i][1] ~= 'rur') then
 			ticker = act_list[i][1];
-			price = myqlua.getPrice(ticker);		-- !!! Р—Р°РјРµРЅРёС‚СЊ РѕР±СЂР°С‚РЅРѕ, РєРѕРіРґР° Р·Р°СЂР°Р±РѕС‚Р°РµС‚ СЃРµСЂРІРµСЂ
+			price = myqlua.getPrice(ticker);		-- !!! Заменить обратно, когда заработает сервер
 			--price = myqlua.getPriceTest(ticker);
 			
 			act_list[i][6] = price;
 			--log.trace(ticker..'('..act_list[i][3]..') - '..price );
 			
-			cnt_share = math.floor(tonumber(act_list[i][2]));		-- РљРѕР»-РІРѕ РЅР° Р±Р°Р»Р°РЅСЃРµ
-			avg_price = tonumber(act_list[i][3]);		-- РЎСЂРµРґРЅСЏСЏ С†РµРЅР° Р»РѕС‚Р°
-			sell_level = act_list[i][4];	-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїСЂРѕРґР°Р¶Рё
-			buy_level = act_list[i][5];		-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїРѕРєСѓРїРєРё
+			cnt_share = math.floor(tonumber(act_list[i][2]));		-- Кол-во на балансе
+			avg_price = tonumber(act_list[i][3]);		-- Средняя цена лота
+			sell_level = act_list[i][4];	-- Номер уровня продажи
+			buy_level = act_list[i][5];		-- Номер уровня покупки
 			log.trace("ticker"..ticker
 						.."cnt_share: "..cnt_share
 						.."price: "..price
@@ -105,36 +105,36 @@ function qlua_sma_botUWGN.main()
 					);--[[]]
 			
 			
-			-- Р•СЃР»Рё РµСЃС‚СЊ С‡С‚Рѕ РїСЂРѕРґР°РІР°С‚СЊ, С‚Рѕ СЃСЂР°РІРЅРёРІР°РµС‚СЃСЏ СЃРѕ СЃСЂРµРґРЅРµР№ С†РµРЅРѕР№ РІ РїРѕСЂС‚С„РµР»Рµ
+			-- Если есть что продавать, то сравнивается со средней ценой в портфеле
 			if cnt_share > 0 then
 				if avg_price > 0 then 
-					-- РћРїСЂРµРґРµР»РёС‚СЊ СѓСЃР»РѕРІРёСЏ РїСЂРё РєРѕС‚РѕСЂС‹С… Р±СѓРґРµС‚ СЃРѕРІРµСЂС€РµРЅР° РїСЂРѕРґР°Р¶Р°
+					-- Определить условия при которых будет совершена продажа
 					log.trace("avg_price: "..avg_price.." sell_level: "..sell_level.." PRICE_STEP: "..PRICE_STEP);
-					sell_price = avg_price + avg_price * (sell_level + 1) * PRICE_STEP;	-- Р¦РµРЅР° РїСЂРѕРґР°Р¶Рё	
+					sell_price = avg_price + avg_price * (sell_level + 1) * PRICE_STEP;	-- Цена продажи	
 					--log.trace("sell_price: "..sell_price);
 					act_list[i][8] = sell_price;	
 					
-					-- РћРїСЂРµРґРµР»РёС‚СЊ СѓСЃР»РѕРІРёСЏ РїСЂРё РєРѕС‚РѕСЂС‹С… Р±СѓРґРµС‚ СЃРѕРІРµСЂС€РµРЅР° РїРѕРєСѓРїРєР°
-					buy_price = avg_price - avg_price * (buy_level + 1) * PRICE_STEP;		-- Р¦РµРЅР° РїРѕРєСѓРїРєРё
+					-- Определить условия при которых будет совершена покупка
+					buy_price = avg_price - avg_price * (buy_level + 1) * PRICE_STEP;		-- Цена покупки
 					
 					--log.trace("buy_price: "..buy_price);
 					act_list[i][7] = buy_price;	
 				else
 					log.trace("ERROR. avg_price = 0");
 					
-					-- Р¦РµРЅСѓ РїСЂРѕРґР°Р¶Рё СЃС‚Р°РІР»СЋ Р·Р°РіСЂР°РґРёС‚РµР»СЊРЅСѓСЋ
+					-- Цену продажи ставлю заградительную
 					sell_price = price * 2;
 				end;
 			else
-				-- РІРѕР·РјРѕР¶РЅР° С‚РѕР»СЊРєРѕ РїРѕРєСѓРїРєР°
-				-- РћРїСЂРµРґРµР»РёС‚СЊ СѓСЃР»РѕРІРёСЏ РїСЂРё РєРѕС‚РѕСЂС‹С… Р±СѓРґРµС‚ СЃРѕРІРµСЂС€РµРЅР° РїРѕРєСѓРїРєР°
-				--buy_price = myqlua.getSMA_7d(ticker) -- Р¦РµРЅР° РїРѕРєСѓРїРєРё
-				buy_price = myqlua.getSMA(ticker, INTERVAL_D1, 7)   -- Р¦РµРЅР° РїРѕРєСѓРїРєРё
+				-- возможна только покупка
+				-- Определить условия при которых будет совершена покупка
+				--buy_price = myqlua.getSMA_7d(ticker) -- Цена покупки
+				buy_price = myqlua.getSMA(ticker, INTERVAL_D1, 7)   -- Цена покупки
 				--log.trace("buy_price: "..buy_price);
 				act_list[i][7] = buy_price;	
 				--log.trace("buy_price act_list[i][7]: "..act_list[i][7]);
 				
-				-- Р¦РµРЅСѓ РїСЂРѕРґР°Р¶Рё СЃС‚Р°РІР»СЋ Р·Р°РіСЂР°РґРёС‚РµР»СЊРЅСѓСЋ
+				-- Цену продажи ставлю заградительную
 				sell_price = price * 2;
 			end
 			
@@ -148,18 +148,18 @@ function qlua_sma_botUWGN.main()
 						.."; buy_price: "..myqlua.ifnull(buy_price, "-")
 					);
 			
-			-- Р•СЃР»Рё С‚РµРєСѓС‰Р°СЏ С†РµРЅР° РІС‹С€Рµ С‡РµРј С†РµРЅР° РїСЂРѕРґР°Р¶Рё, С‚Рѕ РїСЂРѕРґР°СЋ
+			-- Если текущая цена выше чем цена продажи, то продаю
 			if myqlua.ifnull(sell_price, 0) > 0 and price > sell_price then 
 				sell_tickerUWGN (ticker) 
 			end	
 			 
-			-- Р•СЃР»Рё С‚РµРєСѓС‰Р°СЏ С†РµРЅР° РЅРёР¶Рµ, С‡РµРј С†РµРЅР° РїРѕРєСѓРїРєРё, С‚Рѕ РїРѕРєСѓРїР°СЋ
+			-- Если текущая цена ниже, чем цена покупки, то покупаю
 			if myqlua.ifnull(buy_price, 0) > 0 and price < buy_price then 
 				lotsize = getParamEx(class_code, ticker, "LOTSIZE").param_value;
 				if amount_rur > (buy_price * lotsize) then
 					buy_tickerUWGN (ticker) 
 				else
-					log.trace("РќРµС‚ РґРµРЅРµРі РЅР° РїРѕРєСѓРїРєСѓ "..ticker)
+					log.trace("Нет денег на покупку "..ticker)
 				end
 			end
 		else
@@ -175,51 +175,51 @@ function qlua_sma_botUWGN.main()
 	return 0
 end;
 
--- Р¤СѓРЅРєС†РёСЏ РїСЂРѕРґР°Р¶Рё РёРЅСЃС‚СЂСѓРјРµРЅС‚Р°
+-- Функция продажи инструмента
 function buy_tickerUWGN (ticker)
-	-- РџРѕСЃС‚Р°РІРёС‚СЊ Р·Р°СЏРІРєСѓ РЅР° РїРѕРєСѓРїРєСѓ
+	-- Поставить заявку на покупку
 	log.trace('BUYING '..ticker);
-	-- РќР°Р№С‚Рё СЃС‚СЂРѕРєСѓ СЃ СЌС‚РёРј РёРЅСЃС‚СЂСѓРјРµРЅС‚РѕРј 
+	-- Найти строку с этим инструментом 
 	qlua_sma_botUWGN.settings()							-- remove
 	log.trace('length act_list: '..#act_list)			-- remove
 	for i = 1, #act_list do
 		log.trace('buying_iter '..i);			-- remove
 		if act_list[i][1] == ticker then
-			cnt_share = act_list[i][2];		-- РљРѕР»-РІРѕ РЅР° Р±Р°Р»Р°РЅСЃРµ
-			avg_price = tonumber(act_list[i][3]);		-- РЎСЂРµРґРЅСЏСЏ С†РµРЅР° Р»РѕС‚Р°
-			sell_level = act_list[i][4];	-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїСЂРѕРґР°Р¶Рё
-			buy_level = act_list[i][5];	-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїРѕРєСѓРїРєРё
+			cnt_share = act_list[i][2];		-- Кол-во на балансе
+			avg_price = tonumber(act_list[i][3]);		-- Средняя цена лота
+			sell_level = act_list[i][4];	-- Номер уровня продажи
+			buy_level = act_list[i][5];	-- Номер уровня покупки
 			sell_price = act_list[i][8];	
 			buy_price = act_list[i][7];		
 			
 			
-			-- РћРїСЂРµРґРµР»РёС‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ Р»РѕС‚РѕРІ РЅР° РїРѕРєСѓРїРєСѓ 
+			-- Определить количество лотов на покупку 
 			cnt_share_to_buy = math.floor(2 ^ buy_level);
 			
-			-- РљРѕСЃС‚С‹Р»СЊ, С‡С‚РѕР±С‹ РїРѕРїР°СЃС‚СЊ РІ С€Р°Рі С†РµРЅС‹
+			-- Костыль, чтобы попасть в шаг цены
 			--buy_price = getParamEx("TQBR", ticker, "pricemax").param_value;
 			buy_price = myqlua.getPrice(ticker)
 			log.trace('buy_price: '..buy_price.." cnt_share_to_buy:"..cnt_share_to_buy.." "..type(cnt_share_to_buy));
 			
-			-- РџРѕСЃС‚Р°РІРёС‚СЊ Р·Р°СЏРІРєСѓ РЅР° РїСЂРѕРґР°Р¶Сѓ
+			-- Поставить заявку на продажу
 			log.trace('cnt_share_to_buy: '..cnt_share_to_buy..' for price'..buy_price);
 			myqlua.buy(ticker, buy_price, cnt_share_to_buy)									-- !!!
 			
-			-- РћР±РЅРѕРІРёС‚СЊ РѕСЃС‚Р°С‚РєРё РІ РїРѕСЂС‚С„РµР»Рµ
+			-- Обновить остатки в портфеле
 			act_list[i][2] = act_list[i][2] + cnt_share_to_buy;
 			
-			-- РћР±РЅРѕРІРёС‚СЊ СЃС‡РµС‚С‡РёРє СѓСЂРѕРІРЅРµР№
-			act_list[i][4] = 0					-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїСЂРѕРґР°Р¶Рё
-			act_list[i][5] = buy_level + 1;		-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїРѕРєСѓРїРєРё
+			-- Обновить счетчик уровней
+			act_list[i][4] = 0					-- Номер уровня продажи
+			act_list[i][5] = buy_level + 1;		-- Номер уровня покупки
 			
 			
 			--log.trace('sell_price: '..sell_price..' lotsize..'..lotsize);
 			
-			-- РЈРјРµРЅСЊС€РёС‚СЊ СЃС‡РµС‚ СЂСѓР±Р»РµР№ РЅР° С†РµРЅСѓ РїРѕРєСѓРїРєРё
+			-- Уменьшить счет рублей на цену покупки
 			lotsize = getParamEx(class_code, ticker, "LOTSIZE").param_value;
 			add_rublesUWGN(-buy_price * lotsize)
 			
-			-- РЎСЂРµРґРЅСЏСЏ С†РµРЅР° РґРѕР»Р¶РЅР° СЃРЅРёР·РёС‚СЊСЃСЏ
+			-- Средняя цена должна снизиться
 			if avg_price == 0 then
 				act_list[i][3] = buy_price;
 			else
@@ -232,36 +232,36 @@ function buy_tickerUWGN (ticker)
 end
 
 
--- Р¤СѓРЅРєС†РёСЏ РїРѕРєСѓРїРєРё РёРЅСЃС‚СЂСѓРјРµРЅС‚Р°
--- Р”РѕР±Р°РІРёС‚СЊ РїСЂРѕРІРµСЂРєСѓ РЅР° РЅР°Р»РёС‡РёРµ РґРѕСЃС‚Р°С‚РѕС‡РЅРѕРіРѕ РєРѕР»РёС‡РµСЃС‚РІР° РґРµРЅРµРі
+-- Функция покупки инструмента
+-- Добавить проверку на наличие достаточного количества денег
 function sell_tickerUWGN (ticker)
 
 	log.trace('CELLING '..ticker);
-	-- РќР°Р№С‚Рё СЃС‚СЂРѕРєСѓ СЃ СЌС‚РёРј РёРЅСЃС‚СЂСѓРјРµРЅС‚РѕРј 
+	-- Найти строку с этим инструментом 
 	for i = 1, #act_list do
 		if act_list[i][1] == ticker then
-			cnt_share = act_list[i][2];		-- РљРѕР»-РІРѕ РЅР° Р±Р°Р»Р°РЅСЃРµ
-			avg_price = act_list[i][3];		-- РЎСЂРµРґРЅСЏСЏ С†РµРЅР° Р»РѕС‚Р°
-			sell_level = act_list[i][4];	-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїСЂРѕРґР°Р¶Рё
-			buy_level = act_list[i][5];		-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїРѕРєСѓРїРєРё
+			cnt_share = act_list[i][2];		-- Кол-во на балансе
+			avg_price = act_list[i][3];		-- Средняя цена лота
+			sell_level = act_list[i][4];	-- Номер уровня продажи
+			buy_level = act_list[i][5];		-- Номер уровня покупки
 			-- sell_price = act_list[i][8];	-- 
-			sell_price = myqlua.getPrice(ticker);	-- Р¦РµРЅСѓ РїСЂРѕРґР°Р¶Рё СЃС‚Р°РІР»СЋ С‚РµРєСѓС‰СѓСЋ
+			sell_price = myqlua.getPrice(ticker);	-- Цену продажи ставлю текущую
 			buy_price = act_list[i][7];		-- 
 			
 			--log.trace('cnt_share: '..cnt_share);
 			
 			
-			-- РћРїСЂРµРґРµР»РёС‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ Р»РѕС‚РѕРІ РЅР° РїСЂРѕРґР°Р¶Сѓ 
+			-- Определить количество лотов на продажу 
 			if cnt_share == '1' then
 				cnt_share_to_cell = 1;
-				-- РћР±РЅСѓР»СЏСЋ СЃСЂРµРґРЅСЋСЋ С†РµРЅСѓ Р»РѕС‚Р°
+				-- Обнуляю среднюю цену лота
 				act_list[i][3] = 0
 			else 
 				cnt_share_to_cell = math.floor(cnt_share / 2);
 			end
 			
 			
-			-- РљРѕСЃС‚С‹Р»СЊ, С‡С‚РѕР±С‹ РїРѕРїР°СЃС‚СЊ РІ С€Р°Рі С†РµРЅС‹
+			-- Костыль, чтобы попасть в шаг цены
 			--sell_price = getParamEx("TQBR", ticker, "pricemin").param_value
 			sec_price_step = getParamEx(class_code, ticker, "SEC_PRICE_STEP").param_value
 			log.trace('sec_price_step: '..sec_price_step);
@@ -271,20 +271,20 @@ function sell_tickerUWGN (ticker)
 			sell_price = sell_price - ostatok
 			log.trace('sell_price: '..sell_price);
 			
-			-- РџРѕСЃС‚Р°РІРёС‚СЊ Р·Р°СЏРІРєСѓ РЅР° РїСЂРѕРґР°Р¶Сѓ
+			-- Поставить заявку на продажу
 			log.trace('cnt_share_to_cell: '..cnt_share_to_cell..' with price '..tostring(sell_price));
 			myqlua.sell(ticker, sell_price, cnt_share_to_cell)								-- !!!
 			
-			-- РЈРІРµР»РёС‡РёС‚СЊ СЃС‡РµС‚ СЂСѓР±Р»РµР№ РЅР° С†РµРЅСѓ РїСЂРѕРґР°Р¶Рё
+			-- Увеличить счет рублей на цену продажи
 			lotsize = getParamEx(class_code, ticker, "LOTSIZE").param_value;
 			add_rublesUWGN(sell_price * lotsize)
 			
-			-- РћР±РЅРѕРІРёС‚СЊ РѕСЃС‚Р°С‚РєРё РІ РїРѕСЂС‚С„РµР»Рµ
+			-- Обновить остатки в портфеле
 			act_list[i][2] = act_list[i][2] - cnt_share_to_cell;
 			
-			-- РћР±РЅРѕРІРёС‚СЊ СЃС‡РµС‚С‡РёРє СѓСЂРѕРІРЅРµР№
-			act_list[i][4] = sell_level		-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїСЂРѕРґР°Р¶Рё
-			act_list[i][5] = 0;				-- РќРѕРјРµСЂ СѓСЂРѕРІРЅСЏ РїРѕРєСѓРїРєРё
+			-- Обновить счетчик уровней
+			act_list[i][4] = sell_level		-- Номер уровня продажи
+			act_list[i][5] = 0;				-- Номер уровня покупки
 			
 		end; 
 	end;
@@ -292,7 +292,7 @@ function sell_tickerUWGN (ticker)
 	save_portfolioUWGN();
 end
 
--- Р¤СѓРЅРєС†РёСЏ РґРѕР±Р°РІР»СЏРµС‚ СЃСѓРјРјСѓ СЂСѓР±Р»РµР№ amnt РЅР° СЃС‡РµС‚
+-- Функция добавляет сумму рублей amnt на счет
 function add_rublesUWGN(amnt)
 
 	prev_amnt = amount_rur
@@ -302,11 +302,11 @@ function add_rublesUWGN(amnt)
 
 end
 
--- Р¤СѓРЅРєС†РёСЏ РґРѕР»Р¶РЅР° СЃРѕС…СЂР°РЅРёС‚СЊ РїРѕСЂС‚С„РµР»СЊ РёР· СЃРїРёСЃРєР° act_list
+-- Функция должна сохранить портфель из списка act_list
 function save_portfolioUWGN(amnt)
 	log.trace("save_portfolio")
 
--- РџС‹С‚Р°РµС‚СЃСЏ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р» РІ СЂРµР¶РёРјРµ "С‡С‚РµРЅРёСЏ/Р·Р°РїРёСЃРё"
+-- Пытается открыть файл в режиме "чтения/записи"
     f = io.open(getScriptPath().."\\"..PORTFOLIO_FILE, "w");
 	
 	f:write('rur;'..amount_rur..';1'..'\n');
@@ -334,7 +334,7 @@ function save_portfolioUWGN(amnt)
    
    
    
--- Р¤СѓРЅРєС†РёСЏ РІРѕР·РІСЂР°С‰Р°РµС‚ РјР°СЃСЃРёРІ СЃС‚СЂРѕРє. СЂР°Р·РґРµР»СЏРµС‚ РІС…РѕРґСЏС‰СѓСЋ СЃС‚СЂРѕРєСѓ inputstr СЂР°Р·РґРµР»РёС‚РµР»СЏРјРё sep
+-- Функция возвращает массив строк. разделяет входящую строку inputstr разделителями sep
 function mysplit(inputstr, sep)
     if sep == nil then
         sep = "%s"
